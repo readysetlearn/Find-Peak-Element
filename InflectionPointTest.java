65008549b9c0dddee212d1427ec30049fa76ff31
readysetlearn <mister_josh@ymail.com>
2016-06-21 18:08:34 -0700

    Added more test methods.

--- InflectionPointTest.java
+++ InflectionPointTest.java
@@ -6,7 +6,7 @@ import junit.framework.JUnit4TestAdapter;
 public class InflectionPointTest {
 	
 	@Test
-	public void findIndex() {
+	public void basicTest() {
 		System.out.println("findIndex");
 		int[] sample = {1,2,3,2,1};
 		int result = InflectionPoint.findIndex(sample);
@@ -14,8 +14,43 @@ public class InflectionPointTest {
 		
 	}
 	
+	@Test
+	public void oneNegNum() {
+		System.out.println("findIndex");
+		int[] sample = {0,1,-1};
+		int result = InflectionPoint.findIndex(sample);
+		assertEquals(2, result);
+		
+	}
+	
+	@Test
+	public void twoNegNum() {
+		System.out.println("findIndex");
+		int[] sample = {-1,0,-1};
+		int result = InflectionPoint.findIndex(sample);
+		assertEquals(2, result);
+		
+	}
+	
+	@Test
+	public void atEnd() {
+		System.out.println("findIndex");
+		int[] sample = {-1,1,2,3,0};
+		int result = InflectionPoint.findIndex(sample);
+		assertEquals(4, result);
+		
+	}
+	
+	@Test
+	public void atFront() {
+		System.out.println("findIndex");
+		int[] sample = {1,2,1,0,-1};
+		int result = InflectionPoint.findIndex(sample);
+		assertEquals(2, result);
+		
+	}
+	
 	public static void main(String[] args) {
-		//junit.swingui.TestRunner.run( new JUnit4TestAdapter(InflectionPoint.class) );
 		junit.textui.TestRunner.run( new JUnit4TestAdapter(InflectionPointTest.class) );
 	}
 }
