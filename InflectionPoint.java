05386dc099a392a33cfb570349d610f6413db9f0
readysetlearn <mister_josh@ymail.com>
2016-06-23 01:55:32 -0700

    Made the method findIndex() more general, as it now can find the first element greater than the previous element in an array that is initially decreasing, instead of just initially increasing arrays and finding the first element that decreases. Added a method to check if array has a single inflection point: checks if array is monotonically increasing then monotonically decreasing or vice-versa. Nothing has been tested yet. The overall logic in the way the methods call each other needs to be refined. It seems logical to first verify a sequence has an inflection point before trying to find it, but part of the work in verifying it has an inflection point is to find the inflection point (but then the remainder of the array could contain more “inflection points” in which case the concept of an inflection point is invalid and the array could just be random numbers).

--- InflectionPoint.java
+++ InflectionPoint.java
@@ -1,4 +1,5 @@
 import java.util.NoSuchElementException;
+import java.util.IllegalArgumentException;
 
 public class InflectionPoint {
 	
@@ -6,24 +7,91 @@ public class InflectionPoint {
 	**CONDITION: array must have at least three elements
 	**RETURN: index of element that is less than the previous element*/
 	public static int findIndex(int[] arr) {
+		findIndex(arr, 'i');
+	}
+	
+	/*INPUT: array of integers, initially monotonically increasing, then monotonically decreasing
+	**OR initially monotonically decreasing, then monotonically increasing.
+	**CONDITION: array must have at least three elements.
+	**RETURN: if initially increasing, index of first element that is less than the previous element;
+	**else if initially decreasing, index of first element that is greater than the previous element.*/
+	public static int findIndex(int[] arr, char direction) {
+		if(direction == 'i') {//initially increasing
+			//skip first 2 elements as they are in increasing order
+			for(int i = 2; i < arr.length; i++) {
+				if(arr[i] < arr[i-1]) {
+					return i;
+				}
+			}
+			
+			throw new NoSuchElementException("sequence is always increasing");
+		}else if(direction == 'd') {//initially decreasing
+			//skip first 2 elements as they are in increasing order
+			for(int i = 2; i < arr.length; i++) {
+				if(arr[i] > arr[i-1]) {
+					return i;
+				}
+			}			
+		} else {
+			throw new IllegalAccessException("invalid parameter given");
+		}
+	}
+	
+	/*return true if and only if arr has (exactly) inflection point*/
+	public static boolean hasInflectionPoint(int[] arr) {
 		if(arr.length < 3) {
-			throw new NoSuchElementException("array is too short");
+			return false;
 		}
 		
+		//check if sequence is always increasing
+		int previous = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] < previous) {
+				return false;
+			}
+			
+			previous = arr[i];
+		}
 		
-		//skip first 2 elements as they are in increasing order
-		for(int i = 2; i < arr.length; i++) {
-			if(arr[i] < arr[i-1]) {
-				return i;
+		//check if sequence is always decreasing
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] > previous) {
+				return false;
+			}
+			
+			previous = arr[i];
+		}
+		
+		char direction = initialDirection(arr);
+		int changeIndex = findIndex(arr, i);
+		//must check to see if changeIndex was only "inclfection point"
+		//e.g. may be several if just a bunch of random numbers
+		for(int i = changeIndex + 1; i < arr.length; i++) {
+			if(direction == 'i') {
+				if(arr[i] > arr[i-1])
+					return false
+			} else if(direction == 'd') {
+				if(arr[i] < arr[i-1])
+					return false
 			}
 		}
 		
-		throw new NoSuchElementException("sequence is always increasing");
+		return true;
 	}
 	
-	/*return true if and only if arr has an inflection point*/
-	public static boolean hasInflectionPoint(int[] arr) {
-		
+	/*return: 'i' if sequence is initially increasing,
+	**'d' if it's initially decreasing*/
+	private static char initialDirection(int[] arr) {
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] == arr[i-1]) {
+				continue;
+			} else {
+				if(arr[i] > arr[i-1])
+					return 'i';//array elements are initially increasing
+				else
+					return 'd';//array elements are initially decreasing
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
